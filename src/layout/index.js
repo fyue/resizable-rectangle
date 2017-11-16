e33ecc408cdaa3924f9f59949e80232b77520c80
fyue(GongJinJun) <544648788@qq.com>
2017-11-16 16:14:04 +0800

    'tmp'

--- src/layout/index.js
+++ src/layout/index.js
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import css from './index.less';
 import {observer, inject} from 'mobx-react';
 
+const {sin, cos, atan, PI, sqrt} = Math;
 
 @inject('store')
 @observer
@@ -13,64 +14,95 @@ class Layout extends React.Component {
     this.state = {};
   }
 
-  //mouseDown event
-  rbResize(e) {
-    const {store} = this.props;
-    store.changeResizeAble(true);
-  }
-
   onChange(e) {
-    //console.log(e.target.value);
     this.props.store.swingAngle(e.target.value);
   }
 
   render() {
     const {store} = this.props;
-    const {currentDeg, x1, x2, x3, x4, y1, y2, y3, y4} = store;
-    const {dltX1, dltY1, dltX2, dltY2, dltX3, dltY3, dltX4, dltY4} = store;
+    const {rotatedAngles, x1, y1, x2, y2, x3, y3} = store;
+    const {x4, y4} = store.properties;
 
-    const path = `${x1 + dltX1},${y1 - dltY1} ${x2 + dltX2},${y2 - dltY2} ${x4 + dltX4},${y4 - dltY4} ${x3 + dltX3},${y3 - dltY3}`;
+    const path = `${x1},${y1} ${x2},${y2} ${x4},${y4} ${x3},${y3}`;
 
     const viewProps = {
       onMouseMove: (e) => {
+        const {x1: X1, y1: Y1, movable} = store;
+        if (movable) {
+          let Cx = e.clientX;
+          let Cy = e.clientY;
+          let deg = rotatedAngles * PI / 180;
 
+          let Lx = Cx - X1;
+          let Ly = Cy - Y1;
+          let gama = atan(Ly / Lx) - deg;
+          let length = sqrt(Lx ** 2 + Ly ** 2);
+          let f2 = length * sin(gama);
+          let f3 = length * cos(gama);
+
+          let P2x = Cx + f2 * sin(deg);
+          let P2y = Cy - f2 * cos(deg);
+
+          let P3x = Cx - f3 * cos(deg);
+          let P3y = Cy - f3 * sin(deg);
+
+          store.changeP2(P2x, P2y);
+          store.changeP3(P3x, P3y);
+        }
       },
+      onMouseUp: () => {
+        if (store.movable) {
+          store.changeResizeAble(false);
+          store.updateInitState();
+        }
+      }
     };
 
     const props1 = {
       style: {
-        left: x1 + dltX1,
-        top: y1 - dltY1,
+        left: x1,
+        top: y1,
       },
     };
     const props2 = {
       style: {
-        left: x2 + dltX2,
-        top: y2 - dltY2,
+        left: x2,
+        top: y2,
       },
     };
     const props3 = {
       style: {
-        left: x3 + dltX3,
-        top: y3 - dltY3,
+        left: x3,
+        top: y3,
       },
     };
     const props4 = {
       style: {
-        left: x4 + dltX4,
-        top: y4 - dltY4,
+        left: x4,
+        top: y4,
       },
-      onMouseDown: this.rbResize.bind(this),
+      onMouseDown: () => {
+        store.changeResizeAble(true);
+      },
+    };
+
+    const inputProps = {
+      type: 'number',
+      value: rotatedAngles,
+      onChange: this.onChange.bind(this),
+      onMouseUp: (e) => {
+        e.stopPropagation();
+      }
     };
 
     return (
-      <div className={css.layout}>
-        <div className={css.divWrapper} {...viewProps}>
+      <div className={css.layout} {...viewProps}>
+        <div className={css.divWrapper}>
           <div {...props1}>1</div>
           <div {...props2}>2</div>
           <div {...props3}>3</div>
           <div {...props4}>4</div>
-          <input type="number" value={currentDeg} onChange={this.onChange.bind(this)}/>
+          <input {...inputProps}/>
         </div>
         <svg className={css.svgWrapper}>
           <polygon points={path}/>
--- src/store/index.js
+++ src/store/index.js
@@ -3,48 +3,83 @@
  */
 import {observable, computed, action} from 'mobx';
 
-const PI = Math.PI;
-const sin = Math.sin;
-const cos = Math.cos;
-const atan = Math.atan;
-
-//const r = Math.sqrt(100 ** 2 + 100 ** 2) / 2;
+const {sin, cos, atan, asin, acos, PI, sqrt} = Math;
 
 export default class Store {
-  @observable dltX1 = 0;
-  @observable dltY1 = 0;
-  @observable dltX2 = 0;
-  @observable dltY2 = 0;
-  @observable dltX3 = 0;
-  @observable dltY3 = 0;
-  @observable dltX4 = 0;
-  @observable dltY4 = 0;
-
-  @observable currentDeg = 0;
   @observable x1 = 0;
   @observable y1 = 0;
   @observable x2 = 0;
   @observable y2 = 0;
   @observable x3 = 0;
   @observable y3 = 0;
-  @observable x4 = 0;
-  @observable y4 = 0;
+
+  @observable initAngleP1 = 0;
+  @observable initAngleP2 = 0;
+  @observable initAngleP3 = 0;
+  @observable initRadius = 0;
+  @observable initOx = 0;
+  @observable initOy = 0;
+
+  @observable rotatedAngles = 0; //angle, not radian
 
   @observable movable = false;
 
   constructor() {
     this.x1 = 100;
     this.y1 = 100;
-    this.x2 = 200;
+    this.x2 = 150;
     this.y2 = 100;
     this.x3 = 100;
-    this.y3 = 300;
-    this.x4 = 200;
-    this.y4 = 300;
+    this.y3 = 200;
+    this.initRadius = this.radius;
+    this.initAngleP1 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius);
+    this.initAngleP2 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12;
+    this.initAngleP3 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12 + PI;
+    this.initOx = (this.x2 + this.x3) / 2;
+    this.initOy = (this.y2 + this.y3) / 2;
+    console.log(this.initRadius, this.initOx, this.initOy);
   }
 
-  get arfa() {
-    return atan(this.recHeight / this.recWidth);
+  angleToRadian(deg) {
+    return deg * PI / 180;
+  }
+
+  get radius() {
+    return sqrt((this.x2 - this.x3) ** 2 + (this.y2 - this.y3) ** 2) / 2;
+  }
+
+  get angleP12() {
+    let L12 = sqrt((this.x2 - this.x1) ** 2 + (this.y2 - this.y1) ** 2);
+    let L23 = this.radius * 2;
+    return 2 * asin(L12 / L23);
+  }
+
+  @computed get properties() {
+    return {
+      x4: this.x2 + this.x3 - this.x1,
+      y4: this.y2 + this.y3 - this.y1,
+      Ox: (this.x2 + this.x3) / 2,
+      Oy: (this.y2 + this.y3) / 2,
+      radius: this.radius,
+      initAngleP1: asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius),
+      initAngleP2: asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12,
+      initAngleP3: asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12 + PI,
+    };
+  }
+
+  @action changeP1(x, y) {
+    this.x1 = x;
+    this.y1 = y;
+  }
+
+  @action changeP2(x, y) {
+    this.x2 = x;
+    this.y2 = y;
+  }
+
+  @action changeP3(x, y) {
+    this.x3 = x;
+    this.y3 = y;
   }
 
   get recWidth() {
@@ -55,35 +90,31 @@ export default class Store {
     return Math.sqrt((this.x3 - this.x1) ** 2 + (this.y3 - this.y1) ** 2);
   }
 
-  get radius() {
-    return Math.sqrt(this.recWidth ** 2 + this.recHeight ** 2) / 2;
-  }
-
   @action changeResizeAble(able) {
     this.movable = able;
   }
 
+  @action updateInitState() {
+    let rotatedAngles = this.rotatedAngles;
+    this.initRadius = this.radius;
+    this.initAngleP1 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) - this.angleToRadian(rotatedAngles);
+    this.initAngleP2 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12 - this.angleToRadian(rotatedAngles);
+    this.initAngleP3 = asin(((this.y2 + this.y3) / 2 - this.y1) / this.radius) + this.angleP12 + PI - this.angleToRadian(rotatedAngles);
+    this.initOx = (this.x2 + this.x3) / 2;
+    this.initOy = (this.y2 + this.y3) / 2;
+    console.log(this.initRadius, this.initOx, this.initOy);
+  }
+
   @action swingAngle(angle) {
-    this.currentDeg = angle;
-    let deg = angle * PI / 180;
-    let r = this.radius;
-    let A = this.arfa;
-    let offsetPoint1 = A;
-    let offsetPoint2 = PI - A;
-    let offsetPoint3 = PI * 2 - A;
-    let offsetPoint4 = PI + A;
-    console.log(r, A);
+    this.rotatedAngles = angle;
 
-    this.dltX1 = r * (cos(offsetPoint1) - cos(offsetPoint1 + deg));
-    this.dltY1 = r * (sin(offsetPoint1 + deg) - sin(offsetPoint1));
+    let radianDlt = this.angleToRadian(angle);
 
-    this.dltX2 = r * (cos(offsetPoint2) - cos(offsetPoint2 + deg));
-    this.dltY2 = r * (sin(offsetPoint2 + deg) - sin(offsetPoint2));
-
-    this.dltX3 = r * (cos(offsetPoint3) - cos(offsetPoint3 + deg));
-    this.dltY3 = r * (sin(offsetPoint3 + deg) - sin(offsetPoint3));
-
-    this.dltX4 = r * (cos(offsetPoint4) - cos(offsetPoint4 + deg));
-    this.dltY4 = r * (sin(offsetPoint4 + deg) - sin(offsetPoint4));
+    this.x1 = this.initOx - this.initRadius * cos(this.initAngleP1 + radianDlt);
+    this.y1 = this.initOy - this.initRadius * sin(this.initAngleP1 + radianDlt);
+    this.x2 = this.initOx - this.initRadius * cos(this.initAngleP2 + radianDlt);
+    this.y2 = this.initOy - this.initRadius * sin(this.initAngleP2 + radianDlt);
+    this.x3 = this.initOx - this.initRadius * cos(this.initAngleP3 + radianDlt);
+    console.log(this.initRadius, this.initOx, this.initOy);
   }
 }
